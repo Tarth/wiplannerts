7058343f0c912091079e92c47012c0db6054e937
MBE <zirax83@gmail.com>
2020-08-06 10:29:51 +0200

    Update datahandler.ts

--- src/datahandler.ts
+++ src/datahandler.ts
@@ -1,19 +1,10 @@
 import { Job } from "./models";
 
 const url = "http://localhost:3003/";
-// const github = "https://api.github.com/users/janbodnar";
 const axios = require("axios").default;
 
 async function getNumberOfFollowers() {
   let res = await axios.get(url);
-
-  // let nOfFollowers = res.data.followers;
-  // let location = res.data.location;
-  // let id = res.data.id;
-
-  // console.log(`# of followers: ${nOfFollowers}`);
-  // console.log(`Location: ${location}`);
-  // console.log(`ID: ${id}`);
 
   console.log(res);
 }
@@ -46,6 +37,7 @@ export const DataHandler = () => {
       starthour,
       startminute
     );
+
     let slutdato = new Date(endyear, endmonth, enddate, endhour, endminute);
 
     let job: Job = {
