4fb0373e262981a2193bf18eccf292c8838a7527
MBE <zirax83@gmail.com>
2020-08-13 12:38:54 +0200

    Update index.tsx
    -Tabmenu working somewhat. Class doesnt change

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Calendar } from "./calendar";
 import { EntryForm } from "./admin";
+import { TabMenu } from "primereact/tabmenu";
 import * as serviceWorker from "./serviceWorker";
+
+const Tabmenu: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<string>("Kalender");
+  let displaycontent;
+
+  const items = [
+    { label: "Kalender", icon: "pi pi-fw pi-home" },
+    { label: "Admin", icon: "pi pi-fw pi-calendar" },
+  ];
+
+  if (activeTab === "Admin") {
+    displaycontent = <EntryForm />;
+  } else {
+    displaycontent = <Calendar />;
+  }
+
+  return (
+    <div>
+      <TabMenu model={items} onTabChange={(x) => setActiveTab(x.value.label)} />
+      {displaycontent}
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calendar />
-    <EntryForm />
+    <Tabmenu />
   </React.StrictMode>,
   document.getElementById("root")
 );
