21550a1254a6bd104542286c4b6181663f68db0a
MBE <zirax83@gmail.com>
2020-10-05 12:36:15 +0200

    init commit

--- src/admin.tsx
+++ src/admin.tsx
@@ -3,11 +3,13 @@ import "./admin.css";
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/nova-light/theme.css";
 import "primereact/resources/primereact.css";
-import { Button } from "primereact/button";
+// import { Button } from "primereact/button";
 import { GetWorkers, GetJobs } from "./datahandler";
 import { Worker, Job } from "./models";
 import { AddJobForm } from "./components/jobform";
 import { JobListBox } from "./components/joblisttable";
+import Button from "@material-ui/core/Button";
+import "fontsource-roboto";
 
 export const EntryForm: React.FC = () => {
   const [workers, setWorkers] = useState<Worker[]>([]);
@@ -59,7 +61,7 @@ export const EntryForm: React.FC = () => {
   return (
     <div className="body">
       <div className="buttongroup">
-        <Button
+        {/* <Button
           className="addJobButton"
           label="Tilføj job"
           icon="pi pi-plus"
@@ -70,7 +72,17 @@ export const EntryForm: React.FC = () => {
           label="Rediger job"
           icon="pi pi-pencil"
           onClick={() => setViews("")}
-        ></Button>
+        ></Button> */}
+        <Button variant="contained" onClick={() => setViews("addjob")}>
+          Tilføj Job
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setViews("")}
+        >
+          Rediger Job
+        </Button>
       </div>
       {view}
     </div>
