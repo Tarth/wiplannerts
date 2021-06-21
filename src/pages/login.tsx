6d0119f283113bde9f66fdac00a2680236eda71a
MBE <zirax83@gmail.com>
2021-06-21 11:14:19 +0200

    checkpoint

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,29 +5,44 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { useStyles } from "../css/login";
 import { LoginProps, LoginResponse } from "../models/models";
-import CircularProgress from '@material-ui/core/CircularProgress';
-import WaveTop from '../css/imgs/wave-top.png';
-import WaveMid from '../css/imgs/wave-mid.png';
-import WaveBot from '../css/imgs/wave-bot.png';
+import CircularProgress from "@material-ui/core/CircularProgress";
+import WaveTop from "../css/imgs/wave-top.png";
+import WaveMid from "../css/imgs/wave-mid.png";
+import WaveBot from "../css/imgs/wave-bot.png";
 
-export const Login: React.FC<LoginProps> = () => {
+export const Login: React.FC = () => {
   const classes = useStyles();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [loginResponse, setLoginResponse] = useState<LoginResponse>({accessToken: "", refreshToken: "", status: 0, statusText: ""});
+  const [loginResponse, setLoginResponse] = useState<LoginResponse>({
+    accessToken: "",
+    refreshToken: "",
+    status: 0,
+    statusText: "",
+  });
 
-  async function LoginResponse () {
+  async function LoginResponse() {
     const returnmsg = await PostLogin(username, password);
-      if (returnmsg.response.data === "User not found"){
-        console.log("User not found")
+    if (returnmsg.hasOwnProperty("response")) {
+      if (returnmsg.response.data === "User not found") {
+        console.log(returnmsg.response.data);
       } else {
-        setLoginResponse({accessToken: returnmsg.data.accessToken, refreshToken: returnmsg.data.refreshToken, status: returnmsg.status, statusText: returnmsg.statusText})
+        console.log(returnmsg.response.data);
       }
+    } else {
+      setLoginResponse({
+        accessToken: returnmsg.data.accessToken,
+        refreshToken: returnmsg.data.refreshToken,
+        status: returnmsg.status,
+        statusText: returnmsg.statusText,
+      });
     }
-  
+  }
+
   useEffect(() => {
     console.log(loginResponse);
+    setIsLoading(false);
   }, [loginResponse]);
 
   return (
@@ -64,26 +79,35 @@ export const Login: React.FC<LoginProps> = () => {
             variant="contained"
             color="primary"
             onClick={async () => {
-              await LoginResponse();
               setIsLoading(true);
-            }}               
+              await LoginResponse();
+            }}
             disabled={isLoading ? true : false}
-            // startIcon={<CircularProgress color="secondary"/>}
-            >
+            startIcon={isLoading ? <CircularProgress size={20} /> : <></>}
+          >
             Log ind
           </Button>
         </Box>
         <div className="waveWrapper waveAnimation">
           <div className="waveWrapperInner bgTop">
-            <div className="wave waveTop" style={{backgroundImage: `url(${WaveTop})`}}></div>
+            <div
+              className="wave waveTop"
+              style={{ backgroundImage: `url(${WaveTop})` }}
+            ></div>
           </div>
           <div className="waveWrapperInner bgMiddle">
-            <div className="wave waveMiddle" style={{backgroundImage: `url(${WaveMid})`}}></div>
+            <div
+              className="wave waveMiddle"
+              style={{ backgroundImage: `url(${WaveMid})` }}
+            ></div>
           </div>
           <div className="waveWrapperInner bgBottom">
-            <div className="wave waveBottom" style={{backgroundImage: `url(${WaveBot})`}}></div>
+            <div
+              className="wave waveBottom"
+              style={{ backgroundImage: `url(${WaveBot})` }}
+            ></div>
           </div>
-          </div>
+        </div>
       </div>
     </>
   );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Calendar } from "../pages/calendar";
 import { Admin } from "../pages/admin";
 import { Login } from "../pages/login";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export const Index: React.FC = () => {
+  const [isLoggedIn, SetIsLoggedIn] = useState(false);
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Login}></Route>
-          <Route path="/calendar" component={Calendar}></Route>
-          <Route path="/admin" component={Admin}></Route>
+          <Route exact path="/">
+            <Login></Login>
+          </Route>
+          <Route path="/calendar">
+            <Calendar></Calendar>
+          </Route>
+          <Route path="/admin">
+            <Admin></Admin>
+          </Route>
         </Switch>
       </BrowserRouter>
     </>
