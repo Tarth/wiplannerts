08288c162112ba8f29c38e92b9ab0416ac3f4dab
MBE <zirax83@gmail.com>
2021-09-23 14:05:09 +0200

    Removed unused useEffect

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Calendar } from "../pages/calendar";
 import { Admin } from "../pages/admin";
 import { Login } from "../pages/login";
